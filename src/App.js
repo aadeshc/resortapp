1e42008afb78dff0a622ca81c0d0ca666a30a628
unknown <aadeshc@yahoo.com>
2020-03-22 11:21:53 +0530

    secons commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
-import Home from "./pages/Home";
-import Rooms from "./pages/Rooms";
+import { Home } from "./pages/Home";
+import { Rooms } from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
-import ErrorPage from "./pages/ErrorPage";
+import { ErrorPage } from "./pages/ErrorPage";
 function App() {
-  return <div>Hello from app</div>;
+  return (
+    <div>
+      <Home></Home>
+      <Rooms></Rooms>
+      <SingleRoom></SingleRoom>
+      <ErrorPage></ErrorPage>
+    </div>
+  );
 }
 
 export default App;
